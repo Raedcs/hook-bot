4b2fedf4d5260f20d6faaaec6561d3502bed2f19
Raedcs <raedxeno@gmail.com>
2026-03-05 06:53:17 +0300

    Add files via upload

---
+++ bothook.js
@@ -0,0 +1,27 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'eu18-free.falixserver.net', // الرابط من صفحة الـ Network عندك
+        port: 20965, // البورت الأساسي للسيرفر
+        username: 'HookBot', 
+        version: '1.21.11' 
+    })
+
+    bot.on('login', () => {
+        console.log('✅ البوت دخل سيرفر Hookara بنجاح!')
+    })
+
+    // حركة بسيطة كل دقيقة لمنع الطرد (Anti-AFK)
+    setInterval(() => {
+        bot.setControlState('jump', true)
+        setTimeout(() => bot.setControlState('jump', false), 500)
+    }, 60000)
+
+    bot.on('end', () => {
+        console.log('❌ انقطع الاتصال، جاري إعادة المحاولة...')
+        setTimeout(createBot, 30000)
+    })
+}
+
+createBot()
